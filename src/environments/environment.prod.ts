9dcf72d7453e284ebf694ea02b45540a398e1790
aravindgcalms <aravind@calms.com.my>
2026-05-14 18:16:59 +0530

    version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appVersion: '1.0.1',
+  appVersion: '1.0.2',
   proURL: '../',
   apiURL: '../app/api/Vims',
   portalApiURL: '../app/api/portal',
