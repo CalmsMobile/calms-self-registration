a5e22a96f945162cfc1a121373cd53b897e3af17
Nizam <nizam@calms.com.my>
2026-05-15 19:30:29 +0530

    fixed mobile ui issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appVersion: '1.0.2',
+  appVersion: '1.0.4',
   proURL: '../',
   apiURL: '../app/api/Vims',
   portalApiURL: '../app/api/portal',
